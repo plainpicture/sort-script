a159533c3bc69006a455786b76becf41ff95210c
Benjamin Vetter <vetter@plainpicture.de>
2016-07-14 12:25:22 +0200

    Tiny fix

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 mvn -Dmaven.test.skip=true clean package || exit 1
 
-mkdir /usr/local/elasticsearch/plugins/sort-script
+mkdir -p /usr/local/elasticsearch/plugins/sort-script
 
 cp resources/plugin-descriptor.properties /usr/local/elasticsearch/plugins/sort-script
 cp target/sort-script-*.jar /usr/local/elasticsearch/plugins/sort-script

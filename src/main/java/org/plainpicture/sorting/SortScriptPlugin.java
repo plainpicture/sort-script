d4cd952bedb8f25709ec8a5a34b4b4d6ca530caf
Benjamin Vetter <vetter@plainpicture.de>
2017-07-26 14:48:04 +0200

    Fixes

--- src/main/java/org/plainpicture/sorting/SortScriptPlugin.java
+++ src/main/java/org/plainpicture/sorting/SortScriptPlugin.java
@@ -2,19 +2,15 @@
 package org.plainpicture.sorting;
 
 import org.elasticsearch.plugins.Plugin;
-import org.elasticsearch.script.ScriptModule;
+import org.elasticsearch.plugins.ScriptPlugin;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.script.NativeScriptFactory;
+import java.util.Arrays;
+import java.util.List;
 
-public class SortScriptPlugin extends Plugin {
-  @Override public String name() {
-    return "sort-script";
-  }
-
-  @Override public String description() {
-    return "sort-script";
-  }
-
-  public void onModule(ScriptModule module) {
-    module.registerScript("sort-script", SortScriptFactory.class);
+public class SortScriptPlugin extends Plugin implements ScriptPlugin {
+  @Override public List<NativeScriptFactory> getNativeScripts() {
+    return Arrays.asList((NativeScriptFactory)new SortScriptFactory());
   }
 }
 

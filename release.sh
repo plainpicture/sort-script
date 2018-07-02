83ff4b959a10089cce08e1eb50be9010735510f0
Benjamin Vetter <vetter@plainpicture.de>
2018-07-02 14:22:16 +0200

    release.sh updated

--- release.sh
+++ release.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 mvn -Dmaven.test.skip=true clean package
+rm -R release
 mkdir -p release/sort-script
 cp resources/plugin-descriptor.properties release/sort-script
 cp target/sort-script-1.0.jar release/sort-script
-tar -c release/sort-script > release/sort-script.tar
+(cd release && tar -c sort-script > sort-script.tar)
 gzip release/sort-script.tar
